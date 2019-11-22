fef444f4fd0713c57380f9795f48903361c0880c
Tracy Ogutu <tracyogutu@gmail.com>
2019-11-22 14:41:40 +0300

    remove start.sh

--- start.sh
+++
@@ -1,6 +0,0 @@
-export MOVIE_API_KEY='6d7b3c11acae661cd9252d66dddb2883'
-export SECRET_KEY='Flask WTF Secret Key'
-export MAIL_USERNAME='cytrachels@gmail.com'
-export MAIL_PASSWORD='crackthecode'
-
-python3.6 manage.py server
