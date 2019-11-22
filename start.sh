3a39963958097fa2ae3379d032adba909ad951c4
Tracy Ogutu <tracyogutu@gmail.com>
2019-11-22 14:34:25 +0300

    add correct template reference to views.py

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MOVIE_API_KEY='6d7b3c11acae661cd9252d66dddb2883'
 export SECRET_KEY='Flask WTF Secret Key'
-export MAIL_USERNAME='tracyogutu@gmail.com'
-export MAIL_PASSWORD='stonesoup'
+export MAIL_USERNAME='cytrachels@gmail.com'
+export MAIL_PASSWORD='crackthecode'
 
 python3.6 manage.py server
